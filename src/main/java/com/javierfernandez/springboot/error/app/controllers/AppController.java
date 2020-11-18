4ff48eb200e9576964ff9197a3576e9558521c7e
jvifdz <javiason@gmail.com>
2020-11-18 12:23:27 +0100

    creadas la vista ver y su implementacion en el metodo handler

--- src/main/java/com/javierfernandez/springboot/error/app/controllers/AppController.java
+++ src/main/java/com/javierfernandez/springboot/error/app/controllers/AppController.java
@@ -1,10 +1,19 @@
 package com.javierfernandez.springboot.error.app.controllers;
 
+import com.javierfernandez.springboot.error.app.models.domain.Usuario;
+import com.javierfernandez.springboot.error.app.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class AppController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
 
     @GetMapping("/index")
     public String index(){
@@ -13,6 +22,16 @@ public class AppController {
          Integer valor = Integer.parseInt("10x");
 
         return "index";
+    }
+
+    @GetMapping("/ver/{id}")
+    public String ver(@PathVariable Integer id, Model model){
+        Usuario usuario =usuarioService.obtenerPorId(id);
+
+        model.addAttribute("usuario",usuario);
+        model.addAttribute("titulo","Detalle usuario: ".concat(usuario.getNombre()));
+
+        return "ver";
     }
 
 }
